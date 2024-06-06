e4e17eda677bcad893c7c52ec7fe56e3da09ad15
Udit Bhaskar <udit.bhaskar.talukdar@gmail.com>
2024-06-07 01:13:29 +0530

    Update detection_accuracy.py

--- modules/performance/detection_accuracy.py
+++ modules/performance/detection_accuracy.py
@@ -46,83 +46,97 @@ def compute_gt_and_pred_objects(
     gt_dict_node = compute_ground_truth_node(data_dict, labels_to_id_dict, old_to_new_label_id_map)
     data_dict, gt_dict_node = grid.select_meas_within_the_grid(data_dict, gt_dict_node)
     data_dict_dyn, node_labels_dict_dyn = select_moving_data(data_dict, gt_dict_node, labels_to_id_dict)
-    adj_dict_dyn = compute_adjacency_information(data_dict_dyn, config_obj.ball_query_eps_square, config_obj.k_number_nearest_points)
 
-    other_features_dyn = np.stack((
-        data_dict_dyn['meas_px'], data_dict_dyn['meas_py'], 
-        data_dict_dyn['meas_vx'], data_dict_dyn['meas_vy']), axis=-1)
-    edge_features_dyn = compute_edge_features(data_dict_dyn, adj_dict_dyn['adj_list'])
-    node_features_dyn = compute_node_features(
-        data_dict_dyn, adj_dict_dyn['degree'], 
-        include_region_confidence = config_obj.include_region_confidence, 
-        min_range = config_obj.grid_min_r, max_range = config_obj.grid_max_r, 
-        min_azimuth = config_obj.grid_min_th, max_azimuth = config_obj.grid_max_th)
+    num_meas = data_dict_dyn['meas_px'].shape[0]
+    if num_meas > 1:
 
-    graph_features = {}
-    graph_features['other_features_dyn'] = torch.from_numpy(other_features_dyn).to(device).to(torch.float32)
-    graph_features['edge_features_dyn'] = torch.from_numpy(edge_features_dyn).to(device).to(torch.float32)
-    graph_features['node_features_dyn'] = torch.from_numpy(node_features_dyn).to(device).to(torch.float32)
-    graph_features['edge_index_dyn'] = torch.from_numpy(adj_dict_dyn['adj_list'] ).to(device).to(torch.int64)
-    graph_features['adj_matrix_dyn'] = torch.from_numpy(adj_dict_dyn['adj_matrix'] ).to(device).to(torch.bool)
+        adj_dict_dyn = compute_adjacency_information(data_dict_dyn, config_obj.ball_query_eps_square, config_obj.k_number_nearest_points)
 
-    gt_labels_dyn = node_labels_dict_dyn['class_labels']
-    cluster_node_idx, cluster_labels = compute_node_idx_for_each_cluster(data_dict_dyn['meas_trackid'], gt_labels_dyn, device)
+        other_features_dyn = np.stack((
+            data_dict_dyn['meas_px'], data_dict_dyn['meas_py'], 
+            data_dict_dyn['meas_vx'], data_dict_dyn['meas_vy']), axis=-1)
+        edge_features_dyn = compute_edge_features(data_dict_dyn, adj_dict_dyn['adj_list'])
+        node_features_dyn = compute_node_features(
+            data_dict_dyn, adj_dict_dyn['degree'], 
+            include_region_confidence = config_obj.include_region_confidence, 
+            min_range = config_obj.grid_min_r, max_range = config_obj.grid_max_r, 
+            min_azimuth = config_obj.grid_min_th, max_azimuth = config_obj.grid_max_th)
 
-    node_cls_predictions, node_offsets_predictions, edge_cls_predictions, \
-    obj_cls_predictions, cluster_members_list = detector(
-        node_features = graph_features['node_features_dyn'],
-        edge_features = graph_features['edge_features_dyn'],
-        other_features = graph_features['other_features_dyn'],
-        edge_index = graph_features['edge_index_dyn'],
-        adj_matrix = graph_features['adj_matrix_dyn'])
+        graph_features = {}
+        graph_features['other_features_dyn'] = torch.from_numpy(other_features_dyn).to(device).to(torch.float32)
+        graph_features['edge_features_dyn'] = torch.from_numpy(edge_features_dyn).to(device).to(torch.float32)
+        graph_features['node_features_dyn'] = torch.from_numpy(node_features_dyn).to(device).to(torch.float32)
+        graph_features['edge_index_dyn'] = torch.from_numpy(adj_dict_dyn['adj_list'] ).to(device).to(torch.int64)
+        graph_features['adj_matrix_dyn'] = torch.from_numpy(adj_dict_dyn['adj_matrix'] ).to(device).to(torch.bool)
 
-    cluster_members_list_pred = cluster_members_list
-    cluster_members_list_gt = cluster_node_idx
+        gt_labels_dyn = node_labels_dict_dyn['class_labels']
+        cluster_node_idx, cluster_labels = compute_node_idx_for_each_cluster(data_dict_dyn['meas_trackid'], gt_labels_dyn, device)
 
-    # predict object class
-    obj_cls_prob = F.softmax(obj_cls_predictions, dim=-1)
-    obj_cls_score, obj_cls_idx = torch.max(obj_cls_prob, dim=-1)
+        node_cls_predictions, node_offsets_predictions, edge_cls_predictions, \
+        obj_cls_predictions, cluster_members_list = detector(
+            node_features = graph_features['node_features_dyn'],
+            edge_features = graph_features['edge_features_dyn'],
+            other_features = graph_features['other_features_dyn'],
+            edge_index = graph_features['edge_index_dyn'],
+            adj_matrix = graph_features['adj_matrix_dyn'])
 
-    px = graph_features['other_features_dyn'][:, 0].detach().cpu().numpy()
-    py = graph_features['other_features_dyn'][:, 1].detach().cpu().numpy()
+        cluster_members_list_pred = cluster_members_list
+        cluster_members_list_gt = cluster_node_idx
 
-    # -----------------------------------------------------------------------------------------------------
-    obj_class_pred = obj_cls_idx.detach().cpu().numpy().tolist()
-    obj_class_gt = cluster_labels.detach().cpu().numpy().tolist()
+        # predict object class
+        obj_cls_prob = F.softmax(obj_cls_predictions, dim=-1)
+        obj_cls_score, obj_cls_idx = torch.max(obj_cls_prob, dim=-1)
 
-    cluster_mean_list_pred, cluster_cov_list_pred, cluster_size_pred \
-        = compute_proposals(cluster_members_list_pred, px, py, detector.meas_noise_cov)
+        px = graph_features['other_features_dyn'][:, 0].detach().cpu().numpy()
+        py = graph_features['other_features_dyn'][:, 1].detach().cpu().numpy()
 
-    cluster_mean_list_gt, cluster_cov_list_gt, cluster_size_gt \
-        = compute_proposals(cluster_members_list_gt, px, py, detector.meas_noise_cov)
-    
-    # -----------------------------------------------------------------------------------------------------
-    # keep only the valid objects
-    filtered_cluster_mean_list_pred = []
-    filtered_cluster_cov_list_pred = []
-    filtered_cluster_size_pred = []
-    filtered_obj_class_pred = []
-    for cluster_mean, cluster_cov, cluster_size, obj_cls in \
-        zip(cluster_mean_list_pred, cluster_cov_list_pred, cluster_size_pred, obj_class_pred):
-        if cluster_size > cluster_size_threshold:
-            filtered_cluster_mean_list_pred.append(cluster_mean)
-            filtered_cluster_cov_list_pred.append(cluster_cov)
-            filtered_cluster_size_pred.append(cluster_size)
-            filtered_obj_class_pred.append(obj_cls)
+        # -----------------------------------------------------------------------------------------------------
+        obj_class_pred = obj_cls_idx.detach().cpu().numpy().tolist()
+        obj_class_gt = cluster_labels.detach().cpu().numpy().tolist()
 
-    # -----------------------------------------------------------------------------------------------------
-    # keep only the valid objects
-    filtered_cluster_mean_list_gt = []
-    filtered_cluster_cov_list_gt = []
-    filtered_cluster_size_gt = []
-    filtered_obj_class_gt = []
-    for cluster_mean, cluster_cov, cluster_size, obj_cls in \
-        zip(cluster_mean_list_gt, cluster_cov_list_gt, cluster_size_gt, obj_class_gt):
-        if cluster_size > 1:
-            filtered_cluster_mean_list_gt.append(cluster_mean)
-            filtered_cluster_cov_list_gt.append(cluster_cov)
-            filtered_cluster_size_gt.append(cluster_size)
-            filtered_obj_class_gt.append(obj_cls)
+        cluster_mean_list_pred, cluster_cov_list_pred, cluster_size_pred \
+            = compute_proposals(cluster_members_list_pred, px, py, detector.meas_noise_cov)
+
+        cluster_mean_list_gt, cluster_cov_list_gt, cluster_size_gt \
+            = compute_proposals(cluster_members_list_gt, px, py, detector.meas_noise_cov)
+        
+        # -----------------------------------------------------------------------------------------------------
+        # keep only the valid objects
+        filtered_cluster_mean_list_pred = []
+        filtered_cluster_cov_list_pred = []
+        filtered_cluster_size_pred = []
+        filtered_obj_class_pred = []
+        for cluster_mean, cluster_cov, cluster_size, obj_cls in \
+            zip(cluster_mean_list_pred, cluster_cov_list_pred, cluster_size_pred, obj_class_pred):
+            if cluster_size > cluster_size_threshold:
+                filtered_cluster_mean_list_pred.append(cluster_mean)
+                filtered_cluster_cov_list_pred.append(cluster_cov)
+                filtered_cluster_size_pred.append(cluster_size)
+                filtered_obj_class_pred.append(obj_cls)
+
+        # -----------------------------------------------------------------------------------------------------
+        # keep only the valid objects
+        filtered_cluster_mean_list_gt = []
+        filtered_cluster_cov_list_gt = []
+        filtered_cluster_size_gt = []
+        filtered_obj_class_gt = []
+        for cluster_mean, cluster_cov, cluster_size, obj_cls in \
+            zip(cluster_mean_list_gt, cluster_cov_list_gt, cluster_size_gt, obj_class_gt):
+            if cluster_size > 1:
+                filtered_cluster_mean_list_gt.append(cluster_mean)
+                filtered_cluster_cov_list_gt.append(cluster_cov)
+                filtered_cluster_size_gt.append(cluster_size)
+                filtered_obj_class_gt.append(obj_cls)
+
+    else: 
+        filtered_obj_class_pred = []
+        filtered_cluster_mean_list_pred = []
+        filtered_cluster_cov_list_pred = []
+        filtered_cluster_size_pred = []
+        filtered_obj_class_gt = []
+        filtered_cluster_mean_list_gt = []
+        filtered_cluster_cov_list_gt = []
+        filtered_cluster_size_gt = []
      
     return {
         'obj_class_pred': filtered_obj_class_pred,
